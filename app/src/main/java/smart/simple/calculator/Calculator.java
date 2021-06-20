9785bf00bddde4d2f99f68a23605ba59aa3cfbeb
iBekas <s345553452S>
2021-06-21 00:00:26 +0300

    Задумка: Создаем HashMap, которой ключи - это операции, значения - собственно значения. Затем, согласно ключю производим операции.

--- app/src/main/java/smart/simple/calculator/Calculator.java
+++ app/src/main/java/smart/simple/calculator/Calculator.java
@@ -1,4 +1,35 @@
 package smart.simple.calculator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Calculator {
+
+    private Map<Character, Float> resultCalculator = new HashMap<>();
+    private float result;
+    private List list;
+
+    public Calculator() {
+    }
+
+    private void initOperations (StringBuilder operations) {
+        this.list = new ArrayList(Collections.singleton(operations));
+        if(list.contains('+')) this.resultCalculator.put('+', (float) 0);
+        //тут еще надо подумать, если первое введное число отрицательное
+        if(list.contains('-')) this.resultCalculator.put('-', (float) 0);
+        if(list.contains('×')) this.resultCalculator.put('×', (float) 0);
+        if(list.contains('/')) this.resultCalculator.put('/', (float) 0);
+        if(list.contains('√')) this.resultCalculator.put('√', (float) 0);
+        if(list.contains('%')) this.resultCalculator.put('%', (float) 0);
+    }
+
+    private void typeOfOperation (StringBuilder operations){
+
+    }
+
+
+
 }
