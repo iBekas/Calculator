64815ab4c61403898ff6fa7f03b33a6a9647a6da
iBekas <s345553452S>
2021-06-21 14:09:08 +0300

    Отображение элементов в обеих ориентаций экрана поправил, на основе твоих макетов.

--- app/src/main/java/smart/simple/calculator/MainActivity.java
+++ app/src/main/java/smart/simple/calculator/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calculator_rubber);
+        setContentView(R.layout.activity_main);
         initButtonAndText();
     }
 
